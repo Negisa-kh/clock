e9321a9640ddcb46537dd44f50c02290bb3947af
Negisa-kh <negisakhoshabari@gmail.com>
2024-07-06 11:25:45 +0330

    project

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode:'class',
+  theme: {
+    extend: {},
+    screens:{
+      'sm1':'315px',
+      'sm': '576px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '992px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1200px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1400px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  plugins: [],
+}
+
